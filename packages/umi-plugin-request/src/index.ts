a868d9dfe4fb87c07c320afe7791e40c963e3314
xiaohuoni <448627663@qq.com>
2019-04-24 16:00:20 +0800

    add umi-plugin-request

---
+++ packages/umi-plugin-request/src/index.ts
@@ -0,0 +1,28 @@
+// ref:
+// - https://umijs.org/plugin/develop.html
+import { join, dirname, basename, extname } from 'path';
+import { readFileSync } from 'fs';
+
+export default function (api, options) {
+
+  // Example: output the webpack config
+  api.addRuntimePluginKey('request');
+  function generateRequest() {
+    const tpl = join(__dirname, './request.js');
+    const tplContent = readFileSync(tpl, 'utf-8');
+    api.writeTmpFile('request.js', tplContent);
+  }
+  api.onGenerateFiles(() => {
+    generateRequest();
+  });
+
+  api.modifyAFWebpackOpts(memo => {
+    return {
+      ...memo,
+      alias: {
+        ...(memo.alias || {}),
+        'alita-request': '@tmp/request',
+      },
+    };
+  });
+}
