5a9f0a14ef0127dd11637f842280eafe84f4ce50
xiaohuoni <448627663@qq.com>
2019-04-26 09:42:10 +0800

    route exclude

--- packages/alita/src/plugins/index.ts
+++ packages/alita/src/plugins/index.ts
@@ -6,7 +6,13 @@ const defaultOptions = {
     dva: true, antd: true, routes: {
       // 规定只有index文件会被识别成路由
       exclude: [
-        /(?<![\s\S]*index\$?\.(js|jsx|ts|tsx)?)$/
+        /(?<![\s\S]*index\$?\.(js|jsx|ts|tsx)?)$/,
+        /model\.(j|t)sx?$/,
+        /\.test\.(j|t)sx?$/,
+        /service\.(j|t)sx?$/,
+        /models\//,
+        /components\//,
+        /services\//
       ]
     },
   },
