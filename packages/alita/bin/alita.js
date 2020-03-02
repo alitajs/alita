f94bba0fef6055e36c0c0b5fb339666af2adada9
xiaohuoni <448627663@qq.com>
2020-03-02 12:09:34 +0800

    feat: use lib cli

--- packages/alita/bin/alita.js
+++ packages/alita/bin/alita.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
 
-const resolveCwd = require('resolve-cwd');
-
-const { name, bin } = require('../package.json');
-const localCLI = resolveCwd.silent(`${name}/${bin['alita']}`);
-if (!process.env.USE_GLOBAL_UMI && localCLI && localCLI !== __filename) {
-  const debug = require('@umijs/utils').createDebug('alita:cli');
-  debug('Using local install of umi');
-  require(localCLI);
-} else {
-  require('../lib/cli');
-}
+require('../lib/cli');
