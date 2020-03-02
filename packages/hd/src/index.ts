48c29d39268d472cbe81872b0627c4da79c5ec07
xiaohuoni <448627663@qq.com>
2020-03-02 17:51:18 +0800

    fix: hd 默认配置更改无效

--- packages/hd/src/index.ts
+++ packages/hd/src/index.ts
@@ -21,40 +21,39 @@ export default (api: IApi) => {
     },
   });
 
-  if (!api.userConfig.hd && (api.config && !api.config.hd)) {
-    return;
+  if (api.userConfig.hd || (api.config && api.config.hd)) {
+    api.modifyDefaultConfig(config => {
+      const draftConfig = config;
+      const { theme, px2rem: configPx2rem } = api.userConfig?.hd || {};
+      draftConfig.theme = {
+        ...(draftConfig.theme || {}),
+        '@hd': '2px',
+        ...(theme || {}),
+      };
+      draftConfig.extraPostCSSPlugins = [
+        ...(draftConfig.extraPostCSSPlugins || []),
+        px2rem({
+          rootValue: 100,
+          minPixelValue: 2,
+          ...(configPx2rem || {}),
+        }),
+      ];
+      return draftConfig;
+    });
+
+    api.addEntryImports(() => {
+      // src/hd.(tsx|ts|jsx|js)
+      const hdFile = getFile({
+        base: api.paths.absSrcPath || '',
+        type: 'javascript',
+        fileNameWithoutExt: 'hd',
+      });
+      return {
+        source: hdFile
+          ? require.resolve(hdFile.path)
+          : require.resolve(join(__dirname, 'template.js')),
+      };
+    });
   }
 
-  api.modifyDefaultConfig(config => {
-    const draftConfig = config;
-    const { theme, px2rem: configPx2rem } = api.userConfig?.hd || {};
-    draftConfig.theme = {
-      ...(draftConfig.theme || {}),
-      '@hd': '2px',
-      ...(theme || {}),
-    };
-    draftConfig.extraPostCSSPlugins = [
-      ...(draftConfig.extraPostCSSPlugins || []),
-      px2rem({
-        rootValue: 100,
-        minPixelValue: 2,
-        ...(configPx2rem || {}),
-      }),
-    ];
-    return draftConfig;
-  });
-
-  api.addEntryImports(() => {
-    // src/hd.(tsx|ts|jsx|js)
-    const hdFile = getFile({
-      base: api.paths.absSrcPath || '',
-      type: 'javascript',
-      fileNameWithoutExt: 'hd',
-    });
-    return {
-      source: hdFile
-        ? require.resolve(hdFile.path)
-        : require.resolve(join(__dirname, 'template.js')),
-    };
-  });
 };
