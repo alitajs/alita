dbce4f3c71920fdf05c357c6c3e59b83a114cc1b
xiaohuoni <448627663@qq.com>
2020-09-15 17:13:36 +0800

    chore: remove externals react

--- packages/umi-presets-alita/src/plugins/defaultConfig.ts
+++ packages/umi-presets-alita/src/plugins/defaultConfig.ts
@@ -31,43 +31,6 @@ export default (api: IApi) => {
     },
     ...api.userConfig
   } as IConfig;
-  defaultOptions.externals = [
-    {
-      // String
-      react: 'window.React',
-      'react-dom': 'window.ReactDOM',
-      // Object
-    },
-    // Function
-    function (context, request, callback) {
-      callback();
-    },
-  ]
-  defaultOptions.scripts = [
-    'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.production.min.js',
-    'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.production.min.js',
-  ]
-  // 优化程度不高
-  // if (process.env.NODE_ENV !== 'development') {
-  //   defaultOptions.externals = {
-  //     ...{
-  //       'react': 'window.React',
-  //       'react-dom': 'window.ReactDOM',
-  //     },
-  //     ...externals
-  //   }
-  //   // 临时方案，scripts 配置会覆盖
-  //   // defaultOptions.scripts = [
-  //   //   'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.production.min.js',
-  //   //   'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.production.min.js',
-  //   // ].concat(scripts);
-  //   api.addHTMLScripts(() => {
-  //     return [
-  //       { src: 'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.production.min.js', },
-  //       { src: 'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.production.min.js', }
-  //     ]
-  //   });
-  // }
   api.modifyDefaultConfig((memo) => {
     return {
       ...memo,
