c8fc8247d0e79e59d2e1d61bc666414452b9ae9c
xiaohuoni <448627663@qq.com>
2021-05-27 09:50:37 +0800

    fix: kill child

--- packages/alita/src/cli.ts
+++ packages/alita/src/cli.ts
@@ -16,7 +16,22 @@ if (p[0] === 'version' || p[0] === '-v' || p[0] === '-version') {
 process.env.ALITA_NOW_COMMAND = p[0];
 
 process.env.ALITA_DIR = dirname(require.resolve('../package'));
+
 const umiBinPath = require.resolve('umi/bin/umi');
-fork(umiBinPath, p, {
+const child = fork(umiBinPath, p, {
   stdio: 'inherit',
+});
+
+// ref:
+// http://nodejs.cn/api/process/signal_events.html
+// https://lisk.io/blog/development/why-we-stopped-using-npm-start-child-processes
+process.on('SIGINT', () => {
+  child.kill('SIGINT');
+  // ref:
+  // https://github.com/umijs/umi/issues/6009
+  process.exit(0);
+});
+process.on('SIGTERM', () => {
+  child.kill('SIGTERM');
+  process.exit(1);
 });
