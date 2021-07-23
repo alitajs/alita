e662af60296bf0159e04536fd12ed0898640245c
xiaohuoni <448627663@qq.com>
2021-07-23 10:59:47 +0800

    chore: network error, republish

--- packages/hd/src/template.tsx
+++ packages/hd/src/template.tsx
@@ -80,9 +80,7 @@ if (typeof document !== 'undefined') {
     }, 300);
   };
   setFontSize();
-  win.addEventListener('resize', () => {
-    setFontSize();
-  });
+  win.addEventListener('resize', setFontSize);
 
   // hd solution for antd-mobile@2
   // ref: https://mobile.ant.design/docs/react/upgrade-notes-cn#%E9%AB%98%E6%B8%85%E6%96%B9%E6%A1%88
