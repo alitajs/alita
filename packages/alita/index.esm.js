9d419273373a8e521dd8472915544cc04cc04ee0
xiaohuoni <448627663@qq.com>
2021-08-24 14:44:46 +0800

    feat: import all from alita

--- packages/alita/index.esm.js
+++ packages/alita/index.esm.js
@@ -1,1 +1,2 @@
+export * from '@@/core/umiExports';
 export * from 'umi';
--- packages/alita/index.js
+++ packages/alita/index.js
@@ -1,3 +1,8 @@
-const umi = require('umi');
-
+let umi = require('umi');
+try {
+  const umiExports = require('@@/core/umiExports');
+  umi = Object.assign(umi, umiExports);
+  console.log('********************************')
+  console.log(umi)
+} catch (e) {}
 module.exports = umi;
