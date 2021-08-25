7ab13ad34f808b09aa317432b3f47e43ce89499d
xiaohuoni <448627663@qq.com>
2021-08-25 17:59:59 +0800

    feat: support import router from alita

--- packages/umi-presets-alita/src/plugins/router/router.ts
+++ packages/umi-presets-alita/src/plugins/router/router.ts
@@ -1,13 +1,24 @@
 import { IApi } from 'umi';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 export default (api: IApi) => {
+  // 生成临时文件
+  api.onGenerateFiles({
+    fn() {
+      const exportsTpl = readFileSync(join(__dirname, 'historyAdapater.tpl'), 'utf-8');
+
+      api.writeTmpFile({
+        path: 'historyAdapater.ts',
+        content: exportsTpl,
+      });
+    }
+  })
   api.addUmiExports(() => {
     return [
       {
-        source: require.resolve(
-          '../../../src/plugins/router/historyAdapater.ts',
-        ),
-        exportAll: true
+        exportAll: true,
+        source: '../historyAdapater',
       },
     ];
   });
