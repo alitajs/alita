8cf64639022affe1833162cd7ad13b8ad2afc0bd
xiaohuoni <448627663@qq.com>
2021-08-25 18:01:03 +0800

    feat: import all from alita (ssr)

--- packages/alita/src/cjs.ts
+++
@@ -1,7 +0,0 @@
-import * as utils from '@umijs/utils';
-import { defineConfig } from './defineConfig';
-
-export * from '@umijs/runtime';
-export * from '@umijs/types';
-export { utils };
-export { defineConfig };
--- packages/alita/src/index.ts
+++ packages/alita/src/index.ts
@@ -1,28 +1,3 @@
+export * from 'umi';
 // @ts-ignore
 export * from '@@/core/umiExports';
-export {
-  ApplyPluginsType,
-  createBrowserHistory,
-  createHashHistory,
-  createMemoryHistory,
-  dynamic,
-  isBrowser,
-  Link,
-  matchPath,
-  MemoryRouter,
-  NavLink,
-  Plugin,
-  Prompt,
-  Redirect,
-  Route,
-  Router,
-  StaticRouter,
-  Switch,
-  useHistory,
-  useLocation,
-  useParams,
-  useRouteMatch,
-  withRouter,
-  __RouterContext,
-} from '@umijs/runtime';
-export type { MemoryHistory } from '@umijs/runtime';
