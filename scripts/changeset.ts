1e3b03bb72ca4798bbc8c342477588251f12b0e0
fz6m <59400654+fz6m@users.noreply.github.com>
2022-01-21 04:33:29 +0000

    feat(changeset): auto refresh ignore pkg list

---
+++ scripts/changeset.ts
@@ -0,0 +1,32 @@
+import { getPackages } from '@manypkg/get-packages';
+import 'zx/globals';
+
+const root = path.join(__dirname, '../');
+const changesetConfig = path.join(__dirname, '../.changeset/config.json');
+
+const getWorkspaces = async () => getPackages(root);
+
+const change = async () => {
+  const ws = await getWorkspaces();
+  const appNames: string[] = [];
+  ws.packages.forEach((submodule) => {
+    const isPrivate = submodule.packageJson?.private;
+    if (isPrivate) {
+      appNames.push(submodule.packageJson.name);
+    }
+  });
+
+  const config = await fs.readJson(changesetConfig, { encoding: 'utf-8' });
+  config.ignore = appNames;
+  await fs.writeFile(changesetConfig, JSON.stringify(config, null, 2), {
+    encoding: 'utf-8',
+  });
+
+  console.log(
+    chalk.green(`[changeset-config]: refresh config ignore list complete`),
+  );
+
+  $`changeset`;
+};
+
+change();
