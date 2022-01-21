f3622db70d13db86652590311b9041d7c7e50cb6
xiaohuoni <448627663@qq.com>
2022-01-21 09:55:41 +0800

    feat: exports types

--- packages/alita/src/features/umiExports.ts
+++ packages/alita/src/features/umiExports.ts
@@ -1,98 +1,52 @@
-import { lodash, winPath } from '@umijs/utils';
-import assert from 'assert';
+import { existsSync, readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
 import { AlitaApi } from '../types';
 
-const reserveLibrarys = ['umi']; // reserve library
-const reserveExportsNames = [
-  'Link',
-  'NavLink',
-  'Redirect',
-  'dynamic',
-  'router',
-  'withRouter',
-  'Route',
-];
-
-export interface IUmiExport {
-  source: string;
-  exportAll?: boolean;
-  specifiers?: any[];
-}
-
-export function generateExports({
-  item,
-  umiExportsHook,
-}: {
-  item: IUmiExport;
-  umiExportsHook: any;
-}) {
-  assert(item.source, 'source should be supplied.');
-  assert(
-    item.exportAll || item.specifiers,
-    'exportAll or specifiers should be supplied.',
-  );
-  assert(
-    !reserveLibrarys.includes(item.source),
-    `${item.source} is reserve library, Please don't use it.`,
-  );
-  if (item.exportAll) {
-    return `export * from '${winPath(item.source)}';`;
-  }
-  assert(
-    Array.isArray(item.specifiers),
-    `specifiers should be Array, but got ${item.specifiers!.toString()}.`,
-  );
-  const specifiersStrArr = item.specifiers!.map((specifier: any) => {
-    if (typeof specifier === 'string') {
-      assert(
-        !reserveExportsNames.includes(specifier),
-        `${specifier} is reserve name, you can use 'exported' to set alias.`,
-      );
-      assert(
-        !umiExportsHook[specifier],
-        `${specifier} is Defined, you can use 'exported' to set alias.`,
-      );
-      umiExportsHook[specifier] = true;
-      return specifier;
-    } else {
-      assert(
-        lodash.isPlainObject(specifier),
-        `Configure item context should be Plain Object, but got ${specifier}.`,
-      );
-      assert(
-        specifier.local && specifier.exported,
-        'local and exported should be supplied.',
-      );
-      return `${specifier.local} as ${specifier.exported}`;
-    }
-  });
-  return `export { ${specifiersStrArr.join(', ')} } from '${winPath(
-    item.source,
-  )}';`;
-}
-
+// TODO: hack 临时用, umi 里肯定会有导出类型的方法
 export default function (api: AlitaApi) {
-  api.registerMethod({ name: 'addUmiExports' });
-  api.onGenerateFiles(async () => {
-    const umiExports = await api.applyPlugins({
-      key: 'addUmiExports',
-      type: api.ApplyPluginsType.add,
-      initialValue: [],
-    });
+  // push @@/exports.ts
+  api.onGenerateFiles({
+    fn: async () => {
+      if (!existsSync(join(api.paths.absTmpPath, 'exports.ts'))) {
+        console.error('exports.ts no find');
+        return;
+      }
+      const exportsStr = readFileSync(
+        join(api.paths.absTmpPath, 'exports.ts'),
+        'utf-8',
+      );
+      const exports = [];
 
-    let umiExportsHook = {} as any; // repeated definition
-    api.writeTmpFile({
-      path: 'core/umiExports.ts',
-      noPluginDir: true,
-      content:
-        umiExports
-          .map((item: IUmiExport) => {
-            return generateExports({
-              item,
-              umiExportsHook,
-            });
-          })
-          .join('\n') + `\n`,
-    });
+      // plugins
+      const plugins = readdirSync(api.paths.absTmpPath).filter((file) => {
+        if (
+          file.startsWith('plugin-') &&
+          (existsSync(join(api.paths.absTmpPath, file, 'index.ts')) ||
+            existsSync(join(api.paths.absTmpPath, file, 'index.tsx')))
+        ) {
+          return true;
+        }
+      });
+      // plugins types.ts
+      exports.push('// plugins types.ts');
+
+      for (const plugin of plugins) {
+        let file: string;
+        if (!existsSync(join(api.paths.absTmpPath, plugin, 'types.d.ts'))) {
+          continue;
+        }
+        file = join(api.paths.absTmpPath, plugin, 'types.d');
+        exports.push(`export * from '${file}';`);
+      }
+
+      api.writeTmpFile({
+        noPluginDir: true,
+        path: 'exports.ts',
+        content: exportsStr + exports.join('\n'),
+      });
+    },
+    // 最后执行，要在 umi 写完 exports.ts 之后执行
+    // umi 编写  exports.ts stage 也是 Infinity，所以顺序是按加载顺序执行的，这个插件再更后面
+    stage: Infinity,
   });
 }
