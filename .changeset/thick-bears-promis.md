99a873611ce8b53e828d5d70637a10e57035a2be
xiaohuoni <448627663@qq.com>
2022-02-09 09:37:07 +0800

    feat: add antd plugin

---
+++ .changeset/thick-bears-promis.md
@@ -0,0 +1,6 @@
+---
+'alita': minor
+---
+
+feat: 按需加载插件
+feat: 添加 antd 插件用于 pc 端
--- packages/alita/src/preset.ts
+++ packages/alita/src/preset.ts
@@ -4,22 +4,30 @@ export default (api: IApi) => {
   api.onStart(() => {
     console.log('Hello alita@3');
   });
+  const plugins = [
+    require.resolve('./features/config/config'),
+    require.resolve('./features/apptype'),
+    require.resolve('./features/umiExports'),
+    require.resolve('@alita/plugins/dist/aconsole'),
+    require.resolve('@alita/plugins/dist/keepalive'),
+    require.resolve('@alita/plugins/dist/mainpath'),
+    require.resolve('@alita/plugins/dist/request'),
+    require.resolve('@alita/plugins/dist/dva'),
+    require.resolve('@alita/plugins/dist/classnames'),
+    require.resolve('@alita/plugins/dist/model'),
+  ];
+  if (api.userConfig.antd) {
+    plugins.push(require.resolve('@alita/plugins/dist/antd'));
+  }
+  if (api.userConfig.appType === 'native') {
+    plugins.push(require.resolve('@alita/native'));
+  }
+  if (api.userConfig.appType !== 'pc') {
+    plugins.push(require.resolve('@alita/plugins/dist/hd'));
+    plugins.push(require.resolve('@alita/plugins/dist/antdmobile'));
+    plugins.push(require.resolve('@alita/plugins/dist/mobile-layout'));
+  }
   return {
-    plugins: [
-      require.resolve('./features/config/config'),
-      require.resolve('./features/apptype'),
-      require.resolve('./features/umiExports'),
-      require.resolve('@alita/plugins/dist/aconsole'),
-      require.resolve('@alita/plugins/dist/antdmobile'),
-      require.resolve('@alita/plugins/dist/hd'),
-      require.resolve('@alita/plugins/dist/keepalive'),
-      require.resolve('@alita/plugins/dist/mainpath'),
-      require.resolve('@alita/plugins/dist/request'),
-      require.resolve('@alita/plugins/dist/mobile-layout'),
-      require.resolve('@alita/plugins/dist/dva'),
-      require.resolve('@alita/plugins/dist/classnames'),
-      require.resolve('@alita/plugins/dist/model'),
-      require.resolve('@alita/native'),
-    ],
+    plugins,
   };
 };
