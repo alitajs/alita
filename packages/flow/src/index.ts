4a4baedd5ce7ebfc5416bbc95b09f794370113e0
xiaohuoni <448627663@qq.com>
2022-03-12 19:12:39 +0800

    feat: page content header footer

--- packages/flow/src/index.ts
+++ packages/flow/src/index.ts
@@ -1,7 +1,11 @@
+export { default as Content } from './components/Content';
 export { ErrorBoundary, withErrorBoundary } from './components/ErrorBoundary';
 export { default as FillContainer } from './components/FillContainer';
 export { default as FlexContent } from './components/FlexContent';
+export { default as Footer } from './components/Footer';
 export { default as For } from './components/For';
+export { default as Header } from './components/Header';
+export { default as Page } from './components/Page';
 export { default as Show } from './components/Show';
 export { default as ShrinkingModule } from './components/ShrinkingModule';
 export { default as Switch, Match } from './components/Switch';
