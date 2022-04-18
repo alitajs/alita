5f93bc077b0411643cf80c7e014de293914f320a
xiaohuoni <448627663@qq.com>
2022-04-18 10:37:49 +0800

    fix: utils nofind

--- packages/umi-presets-alita/src/plugins/features/mainPath.ts
+++ packages/umi-presets-alita/src/plugins/features/mainPath.ts
@@ -1,5 +1,33 @@
 import { IApi } from 'umi';
-import { resetMainPath } from '@alitajs/utils';
+
+function resetMainPath(routes: any[], mainPath: string) {
+  let newPath = mainPath;
+  // 把用户输入/abc/ 转成 /abc
+  if (newPath !== '/' && newPath.slice(-1) === '/') {
+    newPath = newPath.slice(0, -1);
+  }
+  // 把用户输入abc 转成 /abc
+  if (newPath !== '/' && newPath.slice(0, 1) !== '/') {
+    newPath = `/${newPath}`;
+  }
+  return routes.map((element) => {
+    if (element.isResetMainEdit) {
+      return element;
+    }
+    if (element.path === '/' && !element.routes) {
+      element.path = '/index';
+      element.isResetMainEdit = true;
+    }
+    if (element.path === newPath) {
+      element.path = '/';
+      element.isResetMainEdit = true;
+    }
+    if (Array.isArray(element.routes)) {
+      element.routes = resetMainPath(element.routes, mainPath);
+    }
+    return element;
+  });
+}
 
 export default (api: IApi) => {
   api.describe({
