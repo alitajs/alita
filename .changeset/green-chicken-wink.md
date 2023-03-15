d8c3fe166a8b2e76381c6809b0ebdda1c930a7ea
xiaohuoni <xiaohuoni@dingtalk.com>
2023-03-15 20:02:26 +0800

    feat: umi 59

---
+++ .changeset/green-chicken-wink.md
@@ -0,0 +1,9 @@
+---
+'@alita/plugin-lowcode': patch
+'create-alita': patch
+'@alita/plugins': patch
+'@alita/native': patch
+'alita': patch
+---
+
+feat: umi 59
--- packages/alita/bin/alita.js
+++ packages/alita/bin/alita.js
@@ -8,7 +8,8 @@ process.noDeprecation = '1';
 // 关闭 umi 你知道吗 功能
 // TODO: [你知道吗] 这个功能很好，但是有些提示在做配置内收的 alita 里面提示出来，感觉怪怪的。比如推荐 @umijs/max。所以先关掉，想想该怎么处理
 process.env.DID_YOU_KNOW = 'none';
-require('v8-compile-cache');
+// disable since it's conflicted with typescript cjs + dynamic import
+// require('v8-compile-cache');
 require('../dist/cli')
     .run()
     .catch((e) => {
