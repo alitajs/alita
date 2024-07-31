70a6366af7a13934b45a80834868c6021a6aa196
xiaohuoni <xiaohuoni@dingtalk.com>
2024-07-31 17:12:48 +0800

    chore: code style

--- packages/alita/src/features/compatibleMako.ts
+++ packages/alita/src/features/compatibleMako.ts
@@ -4,20 +4,20 @@ import { dirname } from 'path';
 export default (api: IApi) => {
   // 强制关闭
   if (api.userConfig.mako !== false) {
-    // https://github.com/umijs/mako/issues/979
-    const version = require(`${dirname(
-      require.resolve('pdfjs-dist/package.json'),
-    )}/package.json`).version;
-    if (version === '2.1.266') {
-      api.modifyConfig((memo) => {
-        // 可能通过内置默认开启
-        if (api.config.mako) {
+    api.modifyConfig((memo) => {
+      // 可能通过内置默认开启
+      if (api.config.mako) {
+        // https://github.com/umijs/mako/issues/979
+        const version = require(`${dirname(
+          require.resolve('pdfjs-dist/package.json'),
+        )}/package.json`).version;
+        if (version === '2.1.266') {
           memo.alias['pdfjs-dist'] = dirname(
             require.resolve('@lingxiteam/pdfjs-dist/package.json'),
           );
         }
-        return memo;
-      });
-    }
+      }
+      return memo;
+    });
   }
 };
